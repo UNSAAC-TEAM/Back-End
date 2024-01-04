e92df504c46486e4951b73b4bc29b1f6b7a22f49
FranchescoSoto <u201821078@upc.edu.pe>
2024-01-04 12:28:22 -0500

    Feat: Add repositories

---
+++ src/main/java/org/unsaac/es_bim/iam/application/internal/commandservices/UserCommandService.java
@@ -0,0 +1,4 @@
+package org.unsaac.es_bim.iam.application.internal.commandservices;
+
+public class UserCommandService {
+}
---
+++ src/main/java/org/unsaac/es_bim/iam/infrastructure/persistence/jpa/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package org.unsaac.es_bim.iam.infrastructure.persistence.jpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.unsaac.es_bim.iam.domain.model.aggregates.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    /*find by username*/
+    Optional<User> findByUsername(String username);
+
+    /* if user exists by username*/
+
+    boolean existsByUsername(String username);
+}
