bf1d2c2acf11120fc3a72fede6fb82c4c5fd1fdb
FranchescoSoto <u201821078@upc.edu.pe>
2024-01-04 16:49:47 -0500

    Feat: Add JwtAuthenticationFIlter

---
+++ src/main/java/org/unsaac/es_bim/iam/JWT/JwtAuthenticationFIlter.java
@@ -0,0 +1,4 @@
+package org.unsaac.es_bim.iam.JWT;
+
+public class JwtAuthenticationFIlter {
+}
--- src/main/java/org/unsaac/es_bim/iam/application/internal/commandservices/UserCommandService.java
+++ src/main/java/org/unsaac/es_bim/iam/application/internal/commandservices/UserCommandService.java
@@ -1,4 +1,23 @@
 package org.unsaac.es_bim.iam.application.internal.commandservices;
 
-public class UserCommandService {
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.springframework.stereotype.Service;
+import org.unsaac.es_bim.iam.domain.model.aggregates.User;
+import org.unsaac.es_bim.iam.domain.model.commands.user.SignInCommand;
+import org.unsaac.es_bim.iam.domain.model.commands.user.SignUpCommand;
+import org.unsaac.es_bim.iam.domain.services.user.IUserCommandService;
+
+import java.util.Optional;
+
+@Service
+public class UserCommandService implements IUserCommandService {
+    @Override
+    public Optional<User> handle(SignUpCommand command) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<ImmutablePair<User, String>> handle(SignInCommand command) {
+        return Optional.empty();
+    }
 }
