2b37faf93f6594c34a152c747e52303fae37efb2
FranchescoSoto <u201821078@upc.edu.pe>
2024-01-04 12:13:01 -0500

    Feat: Add services interfaces

--- src/main/java/org/unsaac/es_bim/iam/domain/services/role/IRoleQueryService.java
+++ src/main/java/org/unsaac/es_bim/iam/domain/services/role/IRoleQueryService.java
@@ -1,4 +1,13 @@
 package org.unsaac.es_bim.iam.domain.services.role;
 
+import org.unsaac.es_bim.iam.domain.model.entities.Role;
+import org.unsaac.es_bim.iam.domain.model.queries.role.GetAllRolesQuery;
+import org.unsaac.es_bim.iam.domain.model.queries.role.GetRoleByNameQuery;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IRoleQueryService {
+    List<Role> handle(GetAllRolesQuery query);
+    Optional<Role> handle(GetRoleByNameQuery query);
 }
--- src/main/java/org/unsaac/es_bim/iam/domain/services/user/IUserQueryService.java
+++ src/main/java/org/unsaac/es_bim/iam/domain/services/user/IUserQueryService.java
@@ -1,4 +1,15 @@
 package org.unsaac.es_bim.iam.domain.services.user;
 
+import org.unsaac.es_bim.iam.domain.model.aggregates.User;
+import org.unsaac.es_bim.iam.domain.model.queries.user.GetAllUsersQuery;
+import org.unsaac.es_bim.iam.domain.model.queries.user.GetUserByIdQuery;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IUserQueryService {
+
+    List<User> handle(GetAllUsersQuery query);
+
+    Optional<User> handle(GetUserByIdQuery query);
 }
